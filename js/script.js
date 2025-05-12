73af09b6831a5a55a82f5a3bd11e015d7ab1eebe
Amit Kilambu <amitkilambu8@gmail.com>
2025-05-12 16:03:26 +0545

    added id in section for scroll color changes removed dark mode

--- js/script.js
+++ js/script.js
@@ -24,10 +24,12 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 });
 
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
 window.addEventListener("load", function () {
   if (window.location.hash) {
-    // Temporarily remove hash and scroll to top
-    const scrollY = window.scrollY;
     const hash = window.location.hash;
     history.replaceState(null, null, " ");
     window.scrollTo(0, 0);
